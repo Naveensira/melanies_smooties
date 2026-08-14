8019a0fe1d306363e0b1f9594ca4866626537c6d
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 12:55:38 +1000

    Update SmoothieFrootAPI.py

--- SmoothieFrootAPI.py
+++ SmoothieFrootAPI.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-smoothiefroot_response = requests.get(https://my.smoothiefroot.com/api/fruit/watermelon")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st =(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
