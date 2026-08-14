3de12c11fc0a2a5519b41f96a4f24f0d0c113dd0
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 14:50:58 +1000

    Update SmoothieFrootAPI.py

--- SmoothieFrootAPI.py
+++ SmoothieFrootAPI.py
@@ -20,9 +20,9 @@ st.write("The name on your smoothy will be", name_on_order)
 from snowflake.snowpark.functions import col
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
 st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.stop()
 Ingredients_list = st.multiselect(
     "choose upto 5 ingredients :"
     , my_dataframe
