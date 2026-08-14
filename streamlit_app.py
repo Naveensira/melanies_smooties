7109ff0c4f859a3a86b3a78ecca4be0e93c321a7
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 11:32:35 +1000

    Update streamlit_app.py
    reverted api request calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+
 
 # Import python packages
 import streamlit as st
