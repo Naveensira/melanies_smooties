0447f0e742b905ff0d5df36c4d255fb4f3ee15b4
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 11:47:58 +1000

    Update SmoothieFrootAPI.py

--- SmoothieFrootAPI.py
+++ SmoothieFrootAPI.py
@@ -1,4 +1,4 @@
-import requests  
+import requests as st
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response.json())
 
