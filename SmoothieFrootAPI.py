f44917a604ed3d4a9941bf59387a09c20a101936
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 16:13:45 +1000

    Update SmoothieFrootAPI.py

--- SmoothieFrootAPI.py
+++ SmoothieFrootAPI.py
@@ -51,7 +51,8 @@ if Ingredients_list:
 
     st.subheader(fruit_chosen + ' Nutrition information')
     # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen) 
+    #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen) 
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{SEARCH_ON}") 
     sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
