bdb59de31748baccf3f0891bb8ed7956f431d3ef
Naveensira <83652747+Naveensira@users.noreply.github.com>
2026-08-14 15:55:48 +1000

    Update SmoothieFrootAPI.py

--- SmoothieFrootAPI.py
+++ SmoothieFrootAPI.py
@@ -21,8 +21,14 @@ from snowflake.snowpark.functions import col
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert the snowpark data frame to a pandas dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 Ingredients_list = st.multiselect(
     "choose upto 5 ingredients :"
     , my_dataframe
